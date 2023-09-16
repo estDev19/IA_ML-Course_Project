d5ce4e9e8fe336d05a12c49ae79f5b05e48c41a1
esteban0803 <estebancastrooviedo@gmail.com>
2023-09-16 17:14:11 -0600

    comments

--- intentoTres.py
+++ intentoTres.py
@@ -1,39 +1,47 @@
-import numpy as np
+# Importación de todas las librerías necesarias, NumPy y pandas para el manejo de datos, Matplotlib para visualización (gráfico),
+# y las clases y funciones necesarias de la biblioteca Surprise, que se utilizarán para la construcción del sistema de recomendación.
+import numpy as np   
 import pandas as pd
-import matplotlib.pyplot as plt  # Importa Matplotlib
+import matplotlib.pyplot as plt  
 from surprise import Dataset, Reader
 from surprise import SVD
 from surprise.model_selection import GridSearchCV, cross_validate
 
-# Cargar los datos desde un archivo CSV
+# Carga de los datos desde un archivo CSV llamado "restautant_data.CSV" usando pd.read_csv de pandas. Se le llama df de "data frame"
 df = pd.read_csv('C:\\Users\\esteb\\Desktop\\IA&ML-Proyecto\\restaurant_data.csv')
 
-# Crear un lector para interpretar las calificaciones en el rango de 1-5
+# Creación  de un lector de surprise para interpretar las calificaciones en un rango de 1-5
 reader = Reader(rating_scale=(1, 5))
 
-# Cargar los datos desde el DataFrame de pandas
-data = Dataset.load_from_df(df[['nombre', 'restaurante', 'calificacion']], reader)
+# Carga de los datos desde el DataFrame de pandas a un dataset de surprise, de manera que surprise los pueda manipular. 
+# Usa los atributos name  = nombre de usuario, restaurant_name = nombre del restaurante y rating = calificación
+data = Dataset.load_from_df(df[['name', 'restaurant_name', 'rating']], reader)
 
-# Definir el modelo SVD
+# Se define el modelo a utilizzar como SVD(), es decir se utilizará el modelo de filtrado colaborativo llamado
+# Singular Value Descomposition = Descomposición de Valores Singulares
 model = SVD()
 
-# Definir un conjunto de hiperparámetros para probar con Grid Search
-param_grid = {'n_factors': [150, 200, 250], 
-              'n_epochs' : [100,200,300],
-              'lr_all': [0.010, 0.015, 0.020],
-              'reg_all': [0.010, 0.020, 0.030]}
+# Definición de un conjunto de hiperparámetros para probar con Grid Search = búsqueda en cuadrícula,
+# para buscar la combinación óptima de hiperparámetros **Cuidado con sobreajuste al modificarlos
+param_grid = {'n_factors': [150, 200, 250],  #Factores latentes, las características ocultas que el modelo aprenderá, prueba entre 150, 200 y 250
+              'n_epochs' : [100,200,300],    #Cantidad de iteraciones (épocas) de entrenamiento que se llevarán a cabo, prueba entre 100, 200, 300 épocas
+              'lr_all': [0.010, 0.015, 0.020], #Tamaño de los pasos que el algoritmo toma para ajustar los parámetros del modelo durante el entrenamiento prueba entre 0.010, 0.015, 0.020
+              'reg_all': [0.010, 0.020, 0.030]} #Regularización de todos los parámetros, para prevenir sobreajustes, penaliza valores extremadamente altos
 
-# Realizar una búsqueda en cuadrícula con validación cruzada para encontrar los mejores hiperparámetros
-grid_search = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=5)
-grid_search.fit(data)
+# Se realizará una búsqueda en cuadrícula con validación cruzada para encontrar los mejores hiperparámetros
+# Divide en 5 folds, uno se usa para prueba y el resto para entrenamiento, una proporción 80/20, 20% de prueba y 80% de entramiento
+grid_search = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=5) #Usa el error cuadrático medio (RMSE) y el error absoluto medio (MAE) para evaluar el rendimiento del modelo
+grid_search.fit(data) #Entrenamiento para buscar la combinación óptima de hiperparámetros parea el modelo
 
-# Obtener los mejores hiperparámetros
-best_params = grid_search.best_params['rmse']
+# Se extraen los mejores hiperparámetros
+best_params = grid_search.best_params['rmse'] #Se extraen de acuerdo a la métrica RMSE, la del error cuadrático medio, para penalizar más los errores más grandes en las predicciones.
 print("Mejores hiperparámetros:", best_params)
 
-# Crear el modelo SVD con los mejores hiperparámetros
-model = SVD(n_factors=best_params['n_factors'], n_epochs=best_params['n_epochs'],
-            lr_all=best_params['lr_all'], reg_all=best_params['reg_all'])
+# Se crea el modelo SVD previamente definido, utilizando los mejores hiperparámetros
+model = SVD(n_factors=best_params['n_factors'],
+             n_epochs=best_params['n_epochs'], 
+             lr_all=best_params['lr_all'], 
+             reg_all=best_params['reg_all'])
 
 # Evaluar el rendimiento del modelo utilizando validación cruzada
 cross_val_results = cross_validate(model, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
@@ -45,10 +53,11 @@ for key, values in cross_val_results.items():
 trainset = data.build_full_trainset()
 model.fit(trainset)
 
+
 # Realizar una recomendación para un usuario específico (por ejemplo, 'Gustavo')
-user_id = 'Gustavo'
-user_items = df[df['nombre'] == user_id]['restaurante'].tolist()
-user_unrated_items = [item for item in df['restaurante'].unique() if item not in user_items]
+user_id = 'Ana'
+user_items = df[df['name'] == user_id]['restaurant_name'].tolist()
+user_unrated_items = [item for item in df['restaurant_name'].unique() if item not in user_items]
 
 recommendations = [(item, model.predict(user_id, item).est) for item in user_unrated_items]
 recommendations.sort(key=lambda x: x[1], reverse=True)
@@ -58,19 +67,20 @@ N = 5  # Puedes ajustar este valor según la cantidad de recomendaciones que des
 top_n_recommendations = recommendations[:N]
 
 print(f'Recomendaciones para {user_id}:')
-for i, (restaurant, score) in enumerate(top_n_recommendations, start=1):
-    print(f"{i}. Restaurante: {restaurant}, Puntuación: {score:.2f}")
+for i, (restaurant_name, score) in enumerate(top_n_recommendations, start=1):
+    print(f"{i}. Restaurante: {restaurant_name}, Puntuación: {score:.2f}")
+
 
 
 #GRAPH
 # Obtener las puntuaciones exactas de las recomendaciones
-puntuaciones_exactas = [score for restaurant, score in top_n_recommendations]
+puntuaciones_exactas = [score for restaurant_name, score in top_n_recommendations]
 
 # Escalar las puntuaciones de 1-5 a una escala de 0-100
 puntuaciones_scaled = [(score - 1) / 4 * 100 for score in puntuaciones_exactas]
 
 # Obtener los nombres de los restaurantes recomendados
-restaurantes = [restaurant for restaurant, score in top_n_recommendations]
+restaurantes = [restaurant_name for restaurant_name, score in top_n_recommendations]
 
 # Ajustar el ancho de las barras
 ancho_barras = 0.4
@@ -108,3 +118,4 @@ plt.xticks(posiciones, restaurantes, rotation=45, ha='right', fontsize=12)
 plt.subplots_adjust(left=0.252, bottom=0.233, right=0.8, top=0.912, wspace=0.2, hspace=0.2)
 
 plt.show()  # Mostrar el gráfico en la ventana
+
